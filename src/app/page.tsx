5be0a08b2c5478cb0ee447702f60220119d7ca03
AKASH-PRASAD7 <akash.csel016@bppimt.ac.in>
2024-03-17 15:19:36 +0530

    Init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Search from "@/components/home/Search";
+import ImageGrid from "@/components/home/ImageGrid";
+export default function Home() {
+  return (
+    <main>
+      <Search />
+      <ImageGrid />
+    </main>
+  );
+}
---
+++ src/components/home/ImageGrid.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ImageCard from "./ImageCard";
+
+const ImageGrid = () => {
+  return (
+    <>
+      <section>
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+      </section>
+    </>
+  );
+};
+
+export default ImageGrid;
